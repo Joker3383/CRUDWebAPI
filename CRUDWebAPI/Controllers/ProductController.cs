a9c7471dbc5271951e09eb2bf967fa074511d9c6
vovko <vovko@DESKTOP-V3N9KSE>
2023-07-07 14:32:56 +0300

    Add project files.

---
+++ CRUDWebAPI/Controllers/ProductController.cs
@@ -0,0 +1,110 @@
+﻿using CRUDWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CRUDWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductContext _dbContext;
+
+        public ProductController(ProductContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            if(_dbContext.Products == null)
+            {
+                return NotFound();
+            }
+
+            return await _dbContext.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            if (_dbContext.Products == null)
+            {
+                return NotFound();
+            }
+            var product = await _dbContext.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            
+            _dbContext.Products.Add(product);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProduct), new { id = product.ID }, product);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutProduct(int id, Product product)
+        {
+            if(id != product.ID)
+            {
+                return BadRequest();
+            }
+            _dbContext.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException) 
+            {
+                if (!ProductAvaible(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Ok();
+
+        }
+
+        private bool ProductAvaible(int id)
+        {
+            return (_dbContext.Products?.Any(p => p.ID == id)).GetValueOrDefault();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            if(_dbContext.Products == null)
+            {
+                return NotFound();
+            }
+            var product = await _dbContext.Products.FindAsync(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Products.Remove(product);
+            await _dbContext.SaveChangesAsync();
+            return Ok();
+        }
+
+    }
+}
---
+++ CRUDWebAPI/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUDWebAPI.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? category { get; set; }
+        public int count { get; set; }
+
+    }
+}
